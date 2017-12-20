0a971eaccb6074af918dc56eedf8f6a0bbdb6d12
Bhiku Bhave <bhavebhiku@gmail.com>
2017-12-20 16:29:07 +0530

    Python Workshop Code Sample Test

---
+++ hello.py
@@ -0,0 +1,122 @@
+'''print("Hello World")
+phrase = "Hello World"
+print (phrase)
+nineto5 = ("workshop")
+myvar = ("Hello World MYvar")
+print (myvar)
+class1 = ("Beginners Python Workshop")
+print (class1)
+x = 4
+y=x * x  # store in variable and print or print inside expression
+print (y)
+#foo
+p,d = 1,2 
+print (c)
+print (d)
+#Swap
+a,b = 1,2
+a,b = b,a
+print (a)
+print (b)
+
+x = 4
+y = x + 1
+x = 2
+print (x, y)
+x , y = 2, 6
+x , y = y, x + 2
+print (x, y)
+
+
+a, b = 2,3 
+c, b = a, c + 1 
+print (a, b, c)  # u may get output the values may be store in memmory initially by using same variable
+
+ipcode = 02132
+#print type(zipcode)
+print93.5 zipcode
+print(type(3.5))
+print(9999999999999999999999999999999999999)
+
+num1 = 15
+num2= 75
+num3 = 20
+num4 = 10
+
+
+e = (num1 + num2) * num3 / num4       
+print ("Value of (num1 + num2) * num3 / num4 is ",  e)
+
+e = ((num1 + num2) * num3) / num4
+
+print ("Value of ((num1 + num2) * num3) / num4 is ",  e)
+
+e = (num1 + num2) * (num3 / num4);    
+print ("Value of (num1 + num2) * (num3 / num4) is ",  e)
+
+e = num1 + (num2 * num3) / num4      
+print ("Value of num1 + (num2 * num3) / num4 is ",  e
+import math
+print(math.pi, math.e)
+import random
+print (random.choice(['Apple', 'Orange', 'Pineapple']))
+
+print ('"Spammers" are born in hell')
+name="BHIKU "
+print(name*10)
+if 10>8:
+    print("Ten is greater then either")
+if "foo" in "foobar":
+    print( "Happy Programming !")
+
+sister = 15
+brother = 15
+if sister > brother:
+    print("Sister is elder")
+elif sister == brother:
+    print( "Twins ?")
+else:
+    print ("Brother is elder")
+    
+name = "Swapneel"
+location = "Gogte Institute of Technology and Engineering, Udyambag"
+time = "9AM"
+print( name + ","  + " You are invited for a hands-on  workshop on the Python programming language" )
+print ("The venue is " + location + " and the workshop will start at " + time)
+
+html = "Test message"
+print( "<b>" + html + "</b>")
+print ("<code>" + html + "</code>")
+print( "<h1>" + html + "</h1>")
+
+loudVoice = "Can you hear me yet?" 
+print (loudVoice.lower())
+
+
+phrase = "the surprise is in here somewhere" 
+print(phrase)
+#print (phrase.find("surprise"))
+#print (phrase.find("ejafjallajökull"))
+print( "the surprise is in here somewhere".find("surprise")+"I put a string in your string".find("string"))
+userInput = input("Hey, what's up? ") 
+print ("You said:", userInput)'''
+tries = 0
+while tries < 3: 
+      password = input("Password: ") 
+      if password == "john": 
+                   break 
+      else: 
+            tries = tries + 1 
+else: 
+
+    print ("Suspicious activity. The authorities have been alerted." )
+    print ("GIT Commit Test")
+
+
+ladybugs = 2600
+trees = 20
+
+if (ladybugs/trees) > 125:
+    print ("OK")
+else:
+    print ("NOT OK")
